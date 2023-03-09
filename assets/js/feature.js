8e4a733539d0c22bd23caf042c96adb94e1b2363
yutingh <yutingh@cw.com.tw>
2023-03-09 10:41:23 +0800

    update: slick-gallery

--- assets/js/feature.js
+++ assets/js/feature.js
@@ -9,14 +9,39 @@ $(document).ready(() => {
     slidesToShow: 1,
     slidesToScroll: 1,
   });
-  $('.slick-gallery').slick({
-    dots: true,
-    arrows: true,
-    infinite: false,
-    speed: 1000,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+  $('.slick-gallery-img').each(function(key) {
+    const sliderIdName = `slick-gallery-img-${key}`;
+	  const sliderNavIdName = `slick-gallery-text-${key}`;
+	  this.id = sliderIdName;
+	  $('.slick-gallery-text')[key].id = sliderNavIdName;
+	  const sliderId = `#${sliderIdName}`;
+	  const sliderNavId = `#${sliderNavIdName}`
+    $(sliderId).slick({
+      dots: true,
+      arrows: false,
+      infinite: false,
+      speed: 1000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade: true,
+      asNavFor: sliderNavId,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            dots: false
+          }
+        },
+      ]
+    });
+    $(sliderNavId).slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      speed: 1000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: sliderId,
+    });
   });
 });
